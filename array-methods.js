8297bc3235a51c1f66b09a706ffd7a7b046d3aa0
Chaz Leong <cleong14@gmail.com>
2015-12-05 18:59:50 -1000

    WIP. currently on sum of high interest

--- array-methods.js
+++ array-methods.js
@@ -5,7 +5,29 @@ var dataset = require('./dataset.json');
   greater than 100000.00
   assign the resulting array to `hundredThousandairs`
 */
+
+// desination array, what we want to fill
 var hundredThousandairs = null;
+
+// bank balance array
+var bankBalancesArray = dataset.bankBalances;
+
+// copy of balances
+var balanceCopiesArray = dataset.bankBalances.map(function (balance) {
+  return {
+    amount: balance.amount,
+    state: balance.state
+  };
+});
+
+// define function to check balance amount
+function ballerBalances (element) {
+  // returns result of the test
+  return element.amount > 100000;
+}
+
+// saves new array given from filter function to desination (hundredThousandairs)
+hundredThousandairs = balanceCopiesArray.filter(ballerBalances);
 
 /*
   set a new key for each object in bankBalances named `rounded`
@@ -18,7 +40,26 @@ var hundredThousandairs = null;
     }
   assign the resulting array to `roundedDollar`
 */
+
+// function to round numbers
+function roundNumbers (amount, roundTo) {
+  return parseFloat(parseFloat(amount).toFixed(roundTo));
+}
+
 var roundedDollar = null;
+
+// function to round dollar amounts
+function roundToDollars (element) {
+  // return object with rounded key
+  return {
+    amount: element.amount,
+    state: element.state,
+    rounded: Math.round(element.amount)
+  };
+}
+
+// roundedDollar is a bankBalanceArray but with rounded key
+roundedDollar = balanceCopiesArray.map(roundToDollars);
 
 /*
   set a the `amount` value for each object in bankBalances
@@ -32,9 +73,32 @@ var roundedDollar = null;
 */
 var roundedDime = null;
 
+// function to round to nearest cents
+function modifyAmount (element) {
+  // var that rounds elements amount to nearest tenth
+  var roundedToDime = parseFloat(element.amount).toFixed(1);
+
+// returns object, need to parseFloat again because toFixed changes back to string
+  return {
+    amount: parseFloat(roundedToDime),
+    state: element.state
+  };
+}
+
+roundedDime = balanceCopiesArray.map(modifyAmount);
+
 // set sumOfBankBalances to the sum of all amounts in bankBalances
 var sumOfBankBalances = null;
 
+// function so return sum of all bankBalances rounded to nearest cent
+function balanceSums (previous, current) {
+  // returns obj that invokes roundNumbers function
+  return {
+    amount: roundNumbers(previous.amount, 2) + roundNumbers(current.amount, 2)
+  };
+}
+
+sumOfBankBalances = balanceCopiesArray.reduce(balanceSums).amount;
 /*
   set sumOfInterests to the sum of the 18.9% interest
   for all amounts in bankBalances
@@ -48,6 +112,30 @@ var sumOfBankBalances = null;
   the result should be rounded to the nearest cent
  */
 var sumOfInterests = null;
+
+// interest that have 18.9% interest
+var interestStates = balanceCopiesArray.filter(function(element) {
+  if (element.state === 'WI' || element.state === 'IL' || element.state === 'WY' || element.state === 'OH' || element.state === 'GA' || element.state === 'DE') {
+    return element;
+  }
+});
+
+
+// function to add interests
+function interestSums (previous, current) {
+  // previous is a number, dont need to add interest on this because itll end up adding it twice
+  var previousStateInterest = parseFloat(previous);
+
+  // current.amount is a number a string, parseFloat to get number, then times by interest
+  var currentStateInterest = parseFloat(current.amount) * 0.189;
+
+  // use rounding function to round to nearts cent
+  return roundNumbers(previousStateInterest + currentStateInterest, 2);
+}
+
+// run reduce and give starting value 0. make sure you ues correct data type. if you were using obj make surew you pass an obj as starting value
+sumOfInterests = interestStates.reduce(interestSums, 0);
+
 
 /*
   set sumOfHighInterests to the sum of the 18.9% interest
@@ -63,7 +151,82 @@ var sumOfInterests = null;
     Delaware
   the result should be rounded to the nearest cent
  */
+
+ // 1. filter out states
+ // 2. find interest on each account
+ // 3. sum of interest on each account for remaining
+ // 4. if sum of interest from single state is > 500000, return that state
 var sumOfHighInterests = null;
+
+// filter out these states and return remaining
+var highInterestStates = balanceCopiesArray.filter(function (element) {
+  if (!(element.state === 'WI' || element.state === 'IL' || element.state === 'WY' || element.state === 'OH' || element.state === 'GA' || element.state === 'DE')) {
+    return element;
+  }
+});
+
+// find interest on each returned state
+var consolidatedArray = [];
+console.log(highInterestStates);
+
+var statePerInterest = highInterestStates.filter(function (element) {
+  var totalObjAmount = 0;
+  for (var i = 0; i < highInterestStates.length; i++) {
+    var currentObjState = highInterestStates[i].state;
+    var currentObjAmount = highInterestStates[i].amount;
+
+    for (var k = 0; k < remainingStates.length; k++) {
+      
+    }
+
+    if (currentObjState === 'ME') {
+      totalObjAmount += parseFloat(currentObjAmount);
+      consolidatedArray.push({ amount: totalObjAmount, state: 'ME'});
+    }
+
+    if (currentObjState === 'FL') {
+      totalObjAmount += parseFloat(currentObjAmount);
+      consolidatedArray.push({ amount: totalObjAmount, state: 'FL'});
+    }
+
+    if (currentObjState === 'AL') {
+      totalObjAmount += parseFloat(currentObjAmount);
+      consolidatedArray.push({ amount: totalObjAmount, state: 'AL'});
+    }
+
+    if (currentObjState === 'OK') {
+      totalObjAmount += parseFloat(currentObjAmount);
+      consolidatedArray.push({ amount: totalObjAmount, state: 'OK'});
+    }
+
+    if (currentObjState === 'UT') {
+      totalObjAmount += parseFloat(currentObjAmount);
+      consolidatedArray.push({ amount: totalObjAmount, state: 'UT'});
+    }
+
+    if (currentObjState === 'UT') {
+      totalObjAmount += parseFloat(currentObjAmount);
+      consolidatedArray.push({ amount: totalObjAmount, state: 'UT'});
+    }
+  }
+  return consolidatedArray;
+});
+
+// filter out states < 50000 and return remaining
+var superHighInterest = highInterestStates.filter(function (element) {
+  if (element.amount > 50000) {
+    return element;
+  }
+});
+
+function highInterestSum (previous, current) {
+  var previousStateInterest = parseFloat(previous);
+  var currentStateInterest = parseFloat(current.amount) * 0.189;
+
+  return roundNumbers(previousStateInterest + currentStateInterest, 2);
+}
+
+sumOfHighInterests = superHighInterest.reduce(highInterestSum, 0);
 
 /*
   aggregate the sum of bankBalance amounts
